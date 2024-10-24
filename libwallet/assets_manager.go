a7d2b964c619405d449cfebc1a7c762c3321de20
dreacot <kennedyizuegbu@yahoo.com>
2024-10-24 21:25:36 +0100

    upgrade mobile wallets db to badgerbd

--- libwallet/assets_manager.go
+++ libwallet/assets_manager.go
@@ -396,6 +396,11 @@ func (mgr *AssetsManager) LogDir() string {
 	return filepath.Join(mgr.params.RootDir, logFileName)
 }
 
+// DBDriver returns the db driver in use
+func (mgr *AssetsManager) DBDriver() string {
+	return mgr.params.DbDriver
+}
+
 // OpenWallets opens all wallets in the assets manager.
 func (mgr *AssetsManager) OpenWallets(startupPassphrase string) error {
 	for _, wallet := range mgr.AllWallets() {
